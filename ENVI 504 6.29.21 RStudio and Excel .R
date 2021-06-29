4da41b95002fdea5da61b6ea3ba605f037a0292d
Anna Vietmeier <annavietmeier@Annas-MacBook-Air.local>
2021-06-29 15:50:15 -0400

    6/29/21

---
+++ ENVI 504 6.29.21 RStudio and Excel .R
@@ -0,0 +1,24 @@
+#T, 6/29/21
+library(e1071)
+library(readr)
+
+#read data, perform basic statistics and visualization
+setwd("/Users/annavietmeier/Downloads") #set working directory
+
+data <- read_csv("2622217.csv") #reads file using loaded readr
+data <- read.csv("2622217.csv") #reads file using RStudio pre-loaded code, not as nice
+
+data$TOBS # $ sign lets you select a specific column 
+
+m <- mean(data$TOBS, na.rm = TRUE) #adding na.rm = TRUE is the default function in Excel
+  #This removes the "NA" from the file. NA is a gap in the data. 
+s <- sd(datq$TOBS, na.rm = TRUE) #standard deviation of the column TOBS. 
+
+sk <- skewness(data$TOBS, na.rm = TRUE) #we have these functions from loading e1071
+kt <- kurtosis(data$TOBS, na.rm = TRUE) #we have this function from loading e1071
+
+h <- hist(data$TOBS, breaks = c(-30, -20, -10, 0, 10, 20, 30)) #plot data in a histogram 
+  #the c is short for concatnat, meaning to stack on on top of another. 
+  #this stacks a bunch of numbers into the same vector
+
+
---
+++ windrose.R
@@ -0,0 +1,21 @@
+library(readr)
+library(ggplot2)
+
+wind <- read_csv("pgh_weather.csv")
+
+dir <- hist(wind$`Wind Dir`)
+br <- 10(c(0:36))
+br <- 10*(c(0:36))
+dir <- hist(wind$`Wind Dir`, breaks = br)
+
+# we need to pull the counts and mids from histogram data and put in a regular
+# data frame for the next part to work
+angle <- dir$mids
+count <- dir$counts
+
+y <- data.frame(angle, count)
+
+ggplot(y, aes(x = angle, y = count)) + #AES = aesthetic
+  geom_col(fill = 'steelblue', color = "steelblue") + 
+  coord_polar(theta = "x", start = 0 ) #theta = angle 
+  #This creats a wind rose
