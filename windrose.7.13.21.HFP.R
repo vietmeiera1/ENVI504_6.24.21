1f4b2b4d4a0fb853fc59eb538d7ebc955e3f6ebe
Anna Vietmeier <annavietmeier@Annas-MacBook-Air.local>
2021-07-15 14:59:36 -0400

    Heinz Field Windrose, yes it works

--- windrose.7.13.21.HFP.R
+++ windrose.7.13.21.HFP.R
@@ -10,8 +10,8 @@ library(RColorBrewer)
 
 ## Heinz Field (HFP)
 hfp <- read_csv("https://duq.box.com/shared/static/2cs6xi81xtcmq4mmi46t0v0ev4f2mehs.csv")
-hfp$date <- as_date(hfp$Timestamp)
-hfp$month <- month(hfp$date)
+#hfp$date <- as_date(hfp$Timestamp)
+#hfp$month <- month(hfp$date)
 hfp$dir <- hfp$`Wind Vane` # degrees
 hfp$spd <- 0.44704 * hfp$Anemometer # converted to m/s from mph, per https://allegheny.weatherstem.com/pitt
 
@@ -34,21 +34,21 @@ ggplot(y, aes(x = angle, y = count)) +
 
 ## NEW METHOD - with speed binning
 # divide by direction & speed 
-speed.bins <- 6 
+speed.bins <- 7 
 dir.bins <- 36
 
 ######How do I decide the speed.bins & dir.bins?
 
 
 
-wind <- array(0, dim = c(speed.bins, dir.bins))
-for (i in 1:nrow(hfc)) {
-  j <- ceiling(hfp$dir[i]/10)
-  k <- ceiling(hfc$spd[i]/2)
-  if(k >6) { #brute force correction for speeds over 12m/s
-    k <- 6
-  }
-  wind[k,j] <- wind[k,j] + 1
+wind <- array(0, dim = c(speed.bins, dir.bins)) #preallocation (for memory, required)
+for (t in 1:nrow(hfp)) {
+  j <- ceiling(hfp$dir[t]/10)
+  i <- ceiling(hfp$spd[t])
+  # if(k >6) { #brute force correction for speeds over 12m/s
+  #   k <- 6
+  # }
+  wind[i,j] <- wind[i,j] + 1
 }
 #preallocate data via array, NA = not a number, 0 = zero
 # 0 + 1 = 1, NA + 1 = NA
@@ -58,8 +58,8 @@ for (i in 1:nrow(hfc)) {
 
 
 # ## Now, form long array rather than wide:
-wind.long <- array(NA, dim = dir.bins*speed.bins)
-speeds <- c(rep("0-2",dir.bins), rep("2-4",dir.bins), rep("4-6",dir.bins), rep("6-8",dir.bins), rep("8-10",dir.bins), rep("above 10",dir.bins)) # be sure to fill in as many as the wind bins in "wind" allocation
+wind.long <- array(NA, dim = dir.bins*speed.bins) #preallocation, NA used because EVERy cell will be replaced 
+speeds <- c(rep("0-1",dir.bins), rep("1-2",dir.bins), rep("2-3",dir.bins), rep("3-4",dir.bins), rep("4-5",dir.bins), rep("6-7",dir.bins)) # be sure to fill in as many as the wind bins in "wind" allocation
 directions <- rep(5+10*(c(0:35)), speed.bins)
 for (i in 1:speed.bins) {
   for (j in 1:dir.bins) {
@@ -72,7 +72,7 @@ rose <- data.frame(directions, speeds, wind.long)
 ggplot(rose, aes(fill = fct_rev(speeds), x = directions, y = wind.long)) +
   labs(caption = paste("Heinz Field")) +
   geom_bar(position="stack", stat="identity") +
-  scale_fill_brewer("Speed (m/s)", palette = "Greens") +
+  scale_fill_brewer("Speed (m/s)", palette = "Oranges") +
   coord_polar(theta = "x", start = 0) +
   scale_x_continuous(breaks = seq(0, 360, 45)) +
   theme_linedraw() +
